a79663f45705a813ffc1de601afbb866f8e3fd83
jakem <mazurkiewiczj@duq.edu>
2024-01-22 19:46:50 -0500

    Added Examples of Docstrings

---
+++ docstrings.py
@@ -0,0 +1,22 @@
+from typing import Union
+import pandas as pd
+
+
+def add (a,b) -> Union[int, float]:
+    """
+    args:
+    a: number 1
+    b: number 2
+
+    :return:
+    Ouput of addition of numbers 1 and 2
+    """
+    return a + b
+
+def create_new_dataframe() -> pd.DataFrame:
+    """
+    returns:
+    pd.DataFrame: dataframe containing dummy data
+    """
+    return
+
