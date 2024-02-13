24f517e0cf92d9cb6d9b53a1247ff01cd4d70976
jakem <mazurkiewiczj@duq.edu>
2024-02-13 13:43:30 -0500

    Checked for missing values

--- DUQ330BIGDATA/Week5/npi_reader.py
+++ DUQ330BIGDATA/Week5/npi_reader.py
@@ -7,6 +7,7 @@ def read(path):
 
     mapper = {
             'NPI': 'npi',
+            'Healthcare Provider Taxonomy Code_1': 'taxonomy_code',
             'Provider Last Name (Legal Name)': 'last_name',
             'Provider First Name': 'first_name',
             'Provider First Line Business Practice Location Address': 'address',
@@ -19,6 +20,10 @@ def read(path):
     df = df.rename(columns=mapper)[mapper.values()]
 
     return df
+
+    #There are 850 missing values for taxonomy_code, address, state, and country
+    #There are 6,259 missing last names and 6,262 missing first names
+    #There are 1,505 missing values for cert_date
 
 if __name__ == '__main__':
     df = read(r"data/npidata_pfile_20240205-20240211.csv")
