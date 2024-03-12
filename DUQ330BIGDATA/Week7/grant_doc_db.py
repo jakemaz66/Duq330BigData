66b9bec6d1c961689525374e8177b5f8890f9aa1
jakem <mazurkiewiczj@duq.edu>
2024-03-12 07:12:15 -0400

    Added Databases

---
+++ DUQ330BIGDATA/Week7/grant_doc_db.py
@@ -0,0 +1,77 @@
+import sqlalchemy
+from DUQ330BIGDATA.Week5 import npi_reader
+from DUQ330BIGDATA.Week2 import read_data_exp
+
+
+def db():
+    """
+    This function creates a sqlalchemy engine connection
+    """
+    engine = sqlalchemy.create_engine(
+        'sqlite:///data/live_test_sqlite.db'
+    )
+
+    conn = engine.connect()
+
+    return conn
+
+def npi_csv_to_db(csv_path: str):
+    """
+    This function creates a relational database out of the npi csv dataset
+    """
+
+    #Make npi data, have to rename columns, match data types, define bridge table, download beekeeper studio
+    df = npi_reader.read(csv_path)
+
+    #Subsetting to desired columns
+    df = df[['last_name', 'forename']]
+
+    #Renaming columns to specified names in database table
+    mapper = {
+        'last_name': 'lastname',
+        'forename': 'forename'
+    }
+    df = df.rename(columns=mapper)[mapper.values()]
+
+    #Translating pandas dataframe to database
+    df.to_sql('npi',
+              db(),
+              if_exists='append',
+              index=False
+              #Big Data
+              #method = 'multi'
+              #chunksize=1000
+              )
+
+    return df
+    
+    
+def grants_csv_to_db(year: int):
+    """
+    This function creates a relational database out of the grants csv dataset
+    """
+
+    #Reading in data
+    df = read_data_exp.read_grants_year(year)
+
+    #Subsetting to desired columns
+    df = df[['last_name', 'forename']]
+
+    #Renaming columns to specified names in database table
+    mapper = {
+        'last_name': 'lastname',
+        'forename': 'forename'
+    }
+    df = df.rename(columns=mapper)[mapper.values()]
+
+    #Translating pandas dataframe to database
+    df.to_sql('grants',
+              db(),
+              if_exists='append',
+              index=False
+              )
+
+
+if __name__ == '__main__':
+    npi_csv_to_db(r"C:\Users\jakem\OneDrive\Documents\Visual Studio 2017\Duq330BigData\data\npidata_pfile_20240205-20240211.csv")
+    grants_csv_to_db(2022)
---
+++ DUQ330BIGDATA/Week7/grant_doc_db_creation.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+#Writing SQL Query
+query = '''
+CREATE TABLE IF NOT EXISTS npi (
+    id INTEGER PRIMARY KEY,
+    lastname VARCHAR(100) NOT NULL,
+    forename VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+'''
+
+conn = sqlite3.connect('data/grant_npi.db')
+cursor = conn.cursor()
+
+version_query = 'select sqlite_version()'
+cursor.execute(version_query)
+
+record = cursor.fetchall()
+print('version is', record)
+
+cursor.close()
